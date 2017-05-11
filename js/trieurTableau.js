3a3ab49d85de376a2e14652d7c8507781902a10e
Alan Brisset <alan2007@laposte.net>
2017-05-11 11:05:55 +0200

    Ajout des fichiers de modification et de suppression des données. Ils sont pour l'instant vierges.

--- js/trieurTableau.js
+++ js/trieurTableau.js
@@ -17,37 +17,47 @@ Vous pouvez utiliser, modifier et redistribuer ce script, à condition de laisse
 */
 function sortTable (tb, n) {
 
-var iter = 0;
-while (!tb.tagName || tb.tagName.toLowerCase()
-!= "table") {
-if (!tb.parentNode) return;
-tb = tb.parentNode;
-}
-if (tb.tBodies && tb.tBodies[0]) tb = tb.tBodies[0];
+  var iter = 0;
 
-/* Tri par sélection
- */
-var reg = /^\d+(\.\d+)?$/g;
-var index = 0, value = null, minvalue = null;
-for (var i= tb.rows.length -1; i >= 0; i -= 1) {
-minvalue
-= value = null;
-index = -1;
-for (var j=i; j >= 0; j -= 1) {
-value = tb.rows[j].cells[n].firstChild.nodeValue;
-if (!isNaN(value)) value = parseFloat(value);
-if
-(minvalue == null || value < minvalue) { index = j; minvalue = value; }
-}
+  while (!tb.tagName || tb.tagName.toLowerCase()  != "table") {
+    if (!tb.parentNode)
+      return;
 
-if (index != -1) {
-var row = tb.rows[index];
-if (row) {
-tb.removeChild(row);
-tb.appendChild(row);
-}}
+    tb = tb.parentNode;
+  }
 
-}
-//fin
 
-}
+  if (tb.tBodies && tb.tBodies[0])
+    tb = tb.tBodies[0];
+
+  /* Tri par sélection  */
+  var reg = /^\d+(\.\d+)?$/g;
+  var index = 0, value = null, minvalue = null;
+
+  for (var i= tb.rows.length -1; i >= 0; i -= 1) {
+    minvalue = value = null;
+    index = -1;
+
+    for (var j=i; j >= 0; j -= 1) {
+      value = tb.rows[j].cells[n].firstChild.nodeValue;
+
+      if (!isNaN(value))
+        value = parseFloat(value);
+      if(minvalue == null || value < minvalue) {
+        index = j;
+        minvalue = value;
+      }
+    }
+
+
+    if (index != -1) {
+      var row = tb.rows[index];
+      if (row) {
+        tb.removeChild(row);
+        tb.appendChild(row);
+      }}
+
+    }
+    //fin
+
+  }
